dae967914f3c0cd456aebae7d837ab0e21ff130b
Pietro Giuffrida <p.giuffrida@crif.com>
2019-07-15 13:07:00 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='customlogger',
+      version='0.1',
+      description='My way python logger configuration',
+      url='https://github.com/pietrogiuffrida/customlogger/',
+      author='Pietro Giuffrida',
+      author_email='pietro.giuffri@gmail.com',
+      license='MIT',
+      packages=['customlogger'],
+      zip_safe=False)
